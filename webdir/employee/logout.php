ae1e05296bf65e962c8ae6d12aba8eb1bfc35e6a
Jcdiem <jcdiem@protonmail.com>
2021-03-18 16:56:50 -0500

    style logout

--- webdir/employee/logout.php
+++ webdir/employee/logout.php
@@ -4,7 +4,11 @@ session_unset();
 session_destroy();
 ?>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Wink Home :: Logout</title>
+</head>
 <body>
-<p>You have been successfully logged out.</p>
+<h1 style="text-align: center;">You have been logged out of your account.</h1>
 </body>
 </html>
