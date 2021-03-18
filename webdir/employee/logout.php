8a4753f3020dabe842cf924c234347f0b05956a0
Jcdiem <jcdiem@protonmail.com>
2021-03-18 17:40:58 -0500

    Finalize logout.php style

--- webdir/employee/logout.php
+++ webdir/employee/logout.php
@@ -7,8 +7,23 @@ session_destroy();
 <head>
     <meta charset="UTF-8">
     <title>Wink Home :: Logout</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+            integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/main.css">
 </head>
-<body>
-<h1 style="text-align: center;">You have been logged out of your account.</h1>
+<body class="flex-column align-content-center">
+<h1 class="text-center font-robot d-flex align-self-center"">You have been logged out of your account.</h1>
+<a href="/employee/login.php" class="d-flex align-self-center">
+    <button class="btn-success btn-lg">
+        Login Page
+    </button>
+</a>
 </body>
 </html>
